1e8b4c9bba502917612f01b1f101bc49f8bd8f77
LucindaChambers <lucindamchambers@gmail.com>
2023-10-16 13:53:05 +0100

    feat(rna-transcription): add completed exercise solution

---
+++ rna-transcription/rna-transcription.ts
@@ -0,0 +1,16 @@
+// DNA -> RNA: G -> C, C -> G, T -> A, A -> U
+
+  export function toRna(dna: string): string {
+    
+    // in case of input other than 'G', 'C', 'T', 'A'
+
+    if (dna.match("[^GCTA]")) {
+      throw Error("Invalid input DNA.");
+    };
+
+    return dna.replaceAll("A", "U").
+            replaceAll("T", "A").
+            replaceAll("G", "X").
+            replaceAll("C", "G"). 
+            replaceAll("X", "C");    
+  }
