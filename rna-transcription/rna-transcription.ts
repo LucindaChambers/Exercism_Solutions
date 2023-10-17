4e98d8b0dfdb4ae1aa2fe12d42214529b92c39e5
LucindaChambers <lucindamchambers@gmail.com>
2023-10-17 13:53:38 +0200

    feat(rna-transcription.ts): change match to test

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,16 +1,16 @@
 // DNA -> RNA: G -> C, C -> G, T -> A, A -> U
 
-  export function toRna(dna: string): string {
-    
-    // in case of input other than 'G', 'C', 'T', 'A'
+export function toRna(dna: string): string {
+  // in case of input other than 'G', 'C', 'T', 'A'
 
-    if (dna.match("[^GCTA]")) {
-      throw Error("Invalid input DNA.");
-    };
-
-    return dna.replaceAll("A", "U").
-            replaceAll("T", "A").
-            replaceAll("G", "X").
-            replaceAll("C", "G"). 
-            replaceAll("X", "C");    
+  if (/[^GCTA]/.test(dna)) {
+    throw Error("Invalid input DNA.");
   }
+
+  return dna
+    .replaceAll("A", "U")
+    .replaceAll("T", "A")
+    .replaceAll("G", "X")
+    .replaceAll("C", "G")
+    .replaceAll("X", "C");
+}
