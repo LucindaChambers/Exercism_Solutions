37d6825fc1269e2f631e708e1e01af59d63f5412
LucindaChambers <lucindamchambers@gmail.com>
2023-10-18 15:22:04 +0200

    feat(darts): exchange else ifs for early return

--- darts/darts.ts
+++ darts/darts.ts
@@ -1,14 +1,14 @@
 export function score(x: number, y: number): number {
-  
   const distanceFromCentre = Math.sqrt(x ** 2 + y ** 2);
 
   if (distanceFromCentre > 10) {
     return 0;
-  } else if (distanceFromCentre > 5) {
-    return 1;
-  } else if (distanceFromCentre > 1) {
-    return 5;
-  } else {
-    return 10;
   }
+  if (distanceFromCentre > 5) {
+    return 1;
+  }
+  if (distanceFromCentre > 1) {
+    return 5;
+  }
+  return 10;
 }
