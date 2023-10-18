09d4422f0f10c76bc629f78e5bf8f67ded779ac8
LucindaChambers <lucindamchambers@gmail.com>
2023-10-18 15:30:40 +0200

    feat(pangram): exchange greater than comparison for equal to

--- pangram/pangram.ts
+++ pangram/pangram.ts
@@ -1,6 +1,6 @@
 export function isPangram(sentence: string): boolean {
   const ALPHABET = /[a-z]/g;
   const lettersPresent = new Set(sentence.toLowerCase().match(ALPHABET));
-  const isCompleteAlphabetPresent = lettersPresent.size >= 26;
+  const isCompleteAlphabetPresent = lettersPresent.size === 26;
   return isCompleteAlphabetPresent;
 }
