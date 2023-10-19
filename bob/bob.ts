f2c0ca5964e897fe8910fab4ec5531e755728b03
LucindaChambers <lucindamchambers@gmail.com>
2023-10-19 14:38:54 +0200

    feat(bob): replace else ifs with early returns

--- bob/bob.ts
+++ bob/bob.ts
@@ -1,25 +1,23 @@
 // checks if last non whitespace character is '?'
 function isQuestion(message: string): boolean {
-  return message.trim().endsWith('?');
+  return message.trim().endsWith("?");
 }
 
-// checks an upper case letter exists and
-// that all letters are uppercase 
+// checks an upper case letter exists, ensuring that the string has alphabetical characters and that all letters are uppercase
 function isUpperCase(message: string): boolean {
   return /[A-Z]/.test(message) && message === message.toUpperCase();
 }
 
 // check for all whitesapce by checking if any non whitespace exist and return negation
 function isAllWhitespace(message: string): boolean {
-  return !(/[a-z0-9]/i.test(message));
+  return !/[a-z0-9]/i.test(message);
 }
 
 export function hey(message: string): string {
-
-  if (isQuestion(message) && isUpperCase(message)) return 'Calm down, I know what I\'m doing!';
-  else if (isQuestion(message)) return 'Sure.';
-  else if (isUpperCase(message)) return 'Whoa, chill out!';
-  else if (isAllWhitespace(message)) return 'Fine. Be that way!';
-  else return 'Whatever.';
-
+  if (isQuestion(message) && isUpperCase(message))
+    return "Calm down, I know what I'm doing!";
+  if (isQuestion(message)) return "Sure.";
+  if (isUpperCase(message)) return "Whoa, chill out!";
+  if (isAllWhitespace(message)) return "Fine. Be that way!";
+  return "Whatever.";
 }
