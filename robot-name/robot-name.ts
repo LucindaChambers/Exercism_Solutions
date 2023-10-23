dcb7a6fd4ae96c01455c8ea39c2eceba307b5a27
LucindaChambers <lucindamchambers@gmail.com>
2023-10-23 16:37:29 +0100

    feat(robot-name): add completed exercise

--- robot-name/robot-name.ts
+++ robot-name/robot-name.ts
@@ -1,15 +1,52 @@
 export class Robot {
-  constructor() {}
+  static usedRobotNames: Set<string> = new Set<string>();
+
+  private robotName: string;
+
+  constructor() {
+    this.robotName = Robot.generateUniqueName();
+  }
 
   public get name(): string {
-    throw new Error('Implement Robot#name')
+    return this.robotName;
   }
 
   public resetName(): void {
-    throw new Error('Implement Robot#resetName')
+    this.robotName = Robot.generateUniqueName();
   }
 
   public static releaseNames(): void {
-    throw new Error('Implement Robot.releaseNames')
+    Robot.usedRobotNames.clear();
+  }
+
+  private static generateRandomLetter(): string {
+    const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const randomLetter = ALPHABET[Math.floor(Math.random() * ALPHABET.length)];
+    return randomLetter;
+  }
+
+  private static generateRandomNumber(): string {
+    const NUMBER = "0123456789";
+    const randomNumber = NUMBER[Math.floor(Math.random() * NUMBER.length)];
+    return randomNumber;
+  }
+
+  private static generateRandomName(): string {
+    const randomName = `${Robot.generateRandomLetter()}${Robot.generateRandomLetter()}${Robot.generateRandomNumber()}${Robot.generateRandomNumber()}${Robot.generateRandomNumber()}`;
+    return randomName;
+  }
+
+  private static generateUniqueName(): string {
+    let newName = "";
+    let nameExists = false;
+
+    do {
+      newName = Robot.generateRandomName();
+      nameExists = Robot.usedRobotNames.has(newName);
+    } while (nameExists);
+
+    Robot.usedRobotNames.add(newName);
+
+    return newName;
   }
 }
